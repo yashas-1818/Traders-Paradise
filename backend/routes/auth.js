5a11617d848c89b4969792af797f68a92395a012
yashas-1818 <yashasgowda14318@gmail.com>
2026-03-12 14:55:41 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/User.js');
 const authMiddleware = require('../middleware/auth');
 
 // Register
